bec9afe16f610ea39921ba782d91bb1170a2b881
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-02-18 10:02:01 +0100

    working eeventlistener

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-function toggleNavbar(x) {}
+let trigger = document.querySelector("#expand");
+
+trigger.addEventListener("click", toggleNavbar);
+
+function toggleNavbar() {
+  document.getElementById("menu").classList.toggle("visible");
+}
